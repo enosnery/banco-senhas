188009568d3bfa458976ec24dbe99cca20fd5662
enosn <enos.silva@staples.com>
2023-10-13 00:50:37 -0300

    implementação das regras restantes de força de senha

--- src/main/java/com/example/bancosenhas/util/CalcSenha.java
+++ src/main/java/com/example/bancosenhas/util/CalcSenha.java
@@ -1,7 +1,9 @@
 package com.example.bancosenhas.util;
 
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 public class CalcSenha {
 
@@ -15,15 +17,23 @@ public class CalcSenha {
     public static int calcForcaSenha(String senha) {
         int forca = 0;
         if (!senha.isEmpty()) {
+            String reversedString = new StringBuilder(senha).reverse().toString();
             int n = senha.length();
             forca = (n * 4);
 
-            forca += ((n - patternCount(REGEX_UPPERCASE, senha)) * 2 );
-            forca += ((n - patternCount(REGEX_LOWERCASE, senha)) * 2 );
-            forca += (patternCount(REGEX_NUMBER, senha) * 4 );
+            if(patternCount(REGEX_UPPERCASE, senha) > 0){
+                forca += ((n - patternCount(REGEX_UPPERCASE, senha)) * 2 );
+            }
+            if(patternCount(REGEX_LOWERCASE, senha) > 0){
+                forca += ((n - patternCount(REGEX_LOWERCASE, senha)) * 2 );
+            }
+            if(patternCount(REGEX_NUMBER, senha) > 0){
+                forca += (patternCount(REGEX_NUMBER, senha) * 4 );
+            }
             forca += (patternCount(REGEX_SYMBOLS, senha) * 6 );
             forca += ((patternMiddleCount(REGEX_SYMBOLS, senha) + patternMiddleCount(REGEX_NUMBER, senha))* 2 );
-            forca += requirementCount(senha);
+            int requirementCount = requirementCount(senha);
+            forca += requirementCount > 3 ? requirementCount : 0;
 
 
             if (senha.matches(REGEX_LETTERS_ONLY)) {
@@ -33,15 +43,39 @@ public class CalcSenha {
             if (senha.matches(REGEX_NUMBER)) {
                 forca -= senha.length();
             }
+            forca -= repeatCharacters(REGEX_UPPERCASE, senha);
             forca -= (patternConsecutiveCount(REGEX_UPPERCASE, senha) * 2 );
             forca -= (patternConsecutiveCount(REGEX_LOWERCASE, senha) * 2 );
             forca -= (patternConsecutiveCount(REGEX_NUMBER, senha) * 2 );
 
-            //TODO implement consecutive chars requirement
-
+            forca -= (sequentialCount(senha) * 3 );
+            forca -= (sequentialCount(reversedString) * 3 );
         }
         return forca;
     }
+
+    private static int repeatCharacters(String regexUppercase, String senha) {
+       char[] chars = senha.toCharArray();
+        HashMap<String, Integer> map = new HashMap<>();
+       for (int i = 0; i < chars.length; i++) {
+           for(int j = 0; j < chars.length; j++) {
+               if(chars[i] == chars[j] && i != j) {
+                   map.merge(String.valueOf(chars[i]), 1, Integer::sum);
+               }
+           }
+       }
+       int count = 1;
+       if(map.isEmpty()) {
+           return 0;
+       }
+       for (Integer value : map.values()) {
+           if(value > 2) {
+               count += value;
+           }
+       }
+       return count;
+    }
+
 
     private static int patternCount(String regex, String senha) {
         Pattern p = Pattern.compile(regex);
@@ -54,12 +88,17 @@ public class CalcSenha {
     }
 
     private static int patternConsecutiveCount(String regex, String senha) {
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(senha);
+        char[] chars = senha.toCharArray();
         int count = 0;
-        while (m.find()) {
-            if(m.group().length() > 1) {
-                count += m.group().length();
+        if(chars.length >= 2) {
+            for (int i = 0; i < chars.length - 1; i++) {
+                boolean p1 = Pattern.matches(regex, String.valueOf(chars[i]));
+                boolean p2 = Pattern.matches(regex, String.valueOf(chars[i+1]));
+                if (p1) {
+                    if(p2) {
+                        count++;
+                    }
+                }
             }
         }
         return count;
@@ -106,6 +145,27 @@ public class CalcSenha {
             count = count * 2;
         }
 
+        return count;
+    }
+
+    private static int sequentialCount(String senha) {
+        int count = 0;
+        char[] chars = senha.toCharArray();
+        HashMap<String, Integer> map = new HashMap<>();
+        if(chars.length >= 3) {
+            for (int i = 0; i < chars.length - 2; i++) {
+                int a1 = chars[i];
+                int a2 = chars[i + 1];
+                int a3 = chars[i + 2];
+                if ((a1 + 1 == a2)
+                    && ( a2 + 1 == a3) ){
+                    map.putIfAbsent(String.valueOf(chars[i] + chars[i + 1] + chars[i + 2]), 1);
+                }
+            }
+        }
+        for (Integer value : map.values()) {
+                count += value;
+        }
         return count;
     }
 }
